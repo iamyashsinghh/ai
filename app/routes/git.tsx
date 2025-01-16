f4858d9ff83f6c4b8f2c9b2bb988833729971fcc
Yash Singh <iamyashsinghh@gmail.com>
2025-01-16 06:37:05 +0530

    ok

--- app/routes/git.tsx
+++ app/routes/git.tsx
@@ -7,7 +7,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Yash' }, { name: 'description', content: 'Talk with Yash ai, an AI assistant by Yash Singh' }];
 };
 
 export async function loader(args: LoaderFunctionArgs) {
